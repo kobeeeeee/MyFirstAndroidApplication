7e52c4834cc16b84a34e904f7f02459f9439a03d
kobeeeeee <386793119@qq.com>
2016-06-25 10:56:51 +0800

    butterKnife包导入

--- MyApplication/app/src/main/java/www/chendanfeng/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/www/chendanfeng/com/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package www.chendanfeng.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Bind(R.id.textView1)
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
