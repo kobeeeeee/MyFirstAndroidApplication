e27cc71f018ff3ff51992d2e52a148f9c06fee59
renaaaa <904751306@qq.com>
2016-06-29 10:27:00 +0800

    no message

--- MyApplication/app/src/main/java/www/chendanfeng/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/www/chendanfeng/com/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.textView1)
     TextView textView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
